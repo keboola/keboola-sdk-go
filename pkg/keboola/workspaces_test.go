46f57e256c81e0dc85debe801dcd852e3f17486c
Martin Vaško <Matovidlo2@gmail.com>
2025-08-01 09:17:26 +0200

    feat: Add missing test

--- pkg/keboola/workspaces_test.go
+++ pkg/keboola/workspaces_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/keboola/go-utils/pkg/testproject"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/keboola/keboola-sdk-go/v2/pkg/keboola"
 )
@@ -43,6 +44,7 @@ func TestWorkspacesCreateAndDeletePython(t *testing.T) {
 	foundInstance := false
 	for _, v := range workspaces {
 		if workspace.Workspace.ID == v.Workspace.ID {
+			require.True(t, v.Workspace.Type == string(keboola.WorkspaceTypePython))
 			foundInstance = true
 			break
 		}
@@ -90,6 +92,54 @@ func TestWorkspacesCreateAndDeleteSnowflake(t *testing.T) {
 	foundInstance := false
 	for _, v := range workspaces {
 		if workspace.Workspace.ID == v.Workspace.ID {
+			require.True(t, v.Workspace.Type == string(keboola.WorkspaceTypeSnowflake))
+			foundInstance = true
+			break
+		}
+	}
+	assert.True(t, foundInstance, "Workspace list did not find created workspace")
+
+	// Delete workspace
+	err = api.DeleteWorkspace(
+		ctx,
+		branch.ID,
+		workspace.Config.ID,
+		workspace.Workspace.ID,
+	)
+	assert.NoError(t, err)
+}
+
+func TestWorkspacesCreateAndDeleteBigQuery(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	_, api := keboola.APIClientForAnEmptyProject(t, ctx, testproject.WithBigQueryBackend())
+
+	// Get default branch
+	branch, err := api.GetDefaultBranchRequest().Send(ctx)
+	assert.NoError(t, err)
+	assert.NotNil(t, branch)
+
+	ctx, cancelFn := context.WithTimeout(ctx, time.Minute*10)
+	defer cancelFn()
+
+	// Create workspace
+	workspace, err := api.CreateWorkspace(
+		ctx,
+		branch.ID,
+		"test-bigquery",
+		keboola.WorkspaceTypeBigQuery,
+		keboola.WithExpireAfterHours(1),
+	)
+	assert.NoError(t, err)
+	assert.NotNil(t, workspace)
+
+	// List workspaces - try to find the one we just created
+	workspaces, err := api.ListWorkspaces(ctx, branch.ID)
+	assert.NoError(t, err)
+	foundInstance := false
+	for _, v := range workspaces {
+		if workspace.Workspace.ID == v.Workspace.ID {
+			require.True(t, v.Workspace.Type == string(keboola.WorkspaceTypeBigQuery))
 			foundInstance = true
 			break
 		}
