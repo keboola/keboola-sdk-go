aeb12991cde2031b57c4764c093e0b570d08b19f
Michal Kozák <michal.kozak@keboola.com>
2026-03-02 14:11:29 +0100

    fix: Lint and vet

--- pkg/keboola/management/test/api_deleted_projects_test.go
+++ pkg/keboola/management/test/api_deleted_projects_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
-	openapiclient "github.com/GIT_USER_ID/GIT_REPO_ID"
+	openapiclient "github.com/keboola/keboola-sdk-go/v2/pkg/keboola/management"
 )
 
 func Test_management_DeletedProjectsAPIService(t *testing.T) {
